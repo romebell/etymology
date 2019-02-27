7bb03823133c6270c0e4451f8b09df5d890043b3
Rome Bell <romebell@me.com>
2019-02-27 15:14:51 -0800

    Add util/session_api_util.js and add #login #signup and #logout functions and test in window

--- frontend/etymology.jsx
+++ frontend/etymology.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 // import { configureStore } from './store/store.js'
 // import Root from './
+import {login, signup, logout} from './utils/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   // let store = configureStore();
   ReactDOM.render(<h1>Welcome to Etymology</h1>, root);
 })
+
+// TESTING 
+// window.login = login;
+// window.signup = signup;
+// window.logout = logout;
